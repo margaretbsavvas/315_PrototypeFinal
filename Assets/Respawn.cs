4afeb45eb3584f73b8206e2918921bf98a2514d4
Margaret <margaretbeaumontsavvas@gmail.com>
2023-03-29 13:03:42 -0400

    progress
    lvl 1 and lvl 2 fixed

---
+++ Assets/Respawn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform RespawnPoint;
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = RespawnPoint.transform.position;
+    }
+
+}
